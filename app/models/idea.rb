32ff4e169661fa349f87a97aafa5c329bfea1578
Alex <alexxnavarretee@gmail.com>
2016-07-18 11:13:33 -0600

    Refactors Idea Model
    Adds a private method to the Idea model to keep it away from repetitive code.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -6,16 +6,16 @@ class Idea < ActiveRecord::Base
   default_scope { order(created_at: :desc) }
 
   def self.upvote(id)
-    quality = self.find(id).quality
-    quality_id = qualities.index(quality)
+    quality_id = self.idea_quality(id)
+
     new_quality_id = quality_id < 2 ? quality_id + 1 : 2
     new_quality = qualities.fetch(new_quality_id,2)
     self.find(id).update(quality: new_quality)
   end
 
   def self.downvote(id)
-    quality = self.find(id).quality
-    quality_id = qualities.index(quality)
+    quality_id = self.idea_quality(id)
+
     new_quality_id = quality_id < 2 ? quality_id - 1 : 1
     new_quality = qualities.fetch(new_quality_id,2)
     self.find(id).update(quality: new_quality)
@@ -23,8 +23,13 @@ class Idea < ActiveRecord::Base
 
   private
 
+  def self.idea_quality(id)
+    quality = self.find(id).quality
+
+    qualities.index(quality)
+  end
+
   def self.qualities
     ['swill','plausible','genius']
   end
-
 end
